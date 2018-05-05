7e96765c50b7720fc68bcdf465156b5f1d2ad98b
Jordan Demeulenaere <j.demeulenaere1@gmail.com>
2018-05-05 22:43:05 +0200

    Adapting build and example

--- processor-example/src/main/kotlin/com/example/krpc/example/Application.kt
+++ processor-example/src/main/kotlin/com/example/krpc/example/Application.kt
@@ -1,60 +1,232 @@
 package com.example.krpc.example
 
-import com.example.krpc.processor.GenerateClient
-import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.FunSpec
-import com.squareup.kotlinpoet.NameAllocator
-import com.squareup.kotlinpoet.ParameterSpec
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.context.request.async.DeferredResult
+import com.example.krpc.core.HttpClient
+import com.example.krpc.core.HttpMethod
+import com.example.krpc.core.HttpRequest
+import com.example.krpc.core.HttpRequestBody
+import com.example.krpc.okhttp.OkHttpClient
+import io.ktor.application.ApplicationCall
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.contentCharset
+import io.ktor.request.contentType
+import io.ktor.request.receiveChannel
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.post
+import io.ktor.routing.routing
+import io.ktor.server.engine.ApplicationEngine
+import io.ktor.server.engine.ApplicationEngineEnvironment
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import kotlinx.coroutines.experimental.io.jvm.javaio.toInputStream
+import kotlinx.coroutines.experimental.runBlocking
+import kotlinx.serialization.KSerialLoader
+import kotlinx.serialization.KSerialSaver
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JSON
+import kotlinx.serialization.protobuf.ProtoBuf
+import java.util.Base64
+import java.util.concurrent.TimeUnit
 
-data class User(val firstName: String)
+@Serializable
+data class User(
+	val id: Int,
+	val firstName: String,
+	val lastName: String
+)
 
-@RestController
-@RequestMapping("/hello")
-@GenerateClient("HelloClient")
-class HelloController {
+@Serializable
+data class GetRequest(val id: Int)
 
-	@RequestMapping("/{pathVariable}", method = [RequestMethod.POST])
-	fun echo(
-		@RequestBody body: String,
-		@RequestParam requestParam: Int?,
-		@RequestHeader(required = false) header: String,
-		@PathVariable pathVariable: Long
-	): String {
-		return """I received:
-			|body: $body
-			|requestParam: $requestParam
-			|header: $header
-			|pathVariable: $pathVariable
-		""".trimMargin()
+@Serializable
+data class GetResponse(val user: User)
+
+interface UserService {
+	companion object;
+
+	suspend fun getUser(request: GetRequest): GetResponse
+}
+
+object UserServiceImpl : UserService {
+	override suspend fun getUser(request: GetRequest): GetResponse {
+		return GetResponse(User(request.id, "Jordan", "Demeulenaere"))
+	}
+}
+
+fun UserService.Companion.create(
+	httpClient: HttpClient,
+	baseUrl: String,
+	serialization: Serialization = Serialization.JSON
+): UserService {
+	return object : UserService {
+		override suspend fun getUser(request: GetRequest): GetResponse {
+			val body = when (serialization) {
+				Serialization.JSON -> HttpRequestBody(
+					JSON.stringify(GetRequest.serializer(), request),
+					Serialization.JSON.mediaType
+				)
+				Serialization.PROTOBUF -> HttpRequestBody(
+					Base64.getEncoder().encodeToString(ProtoBuf.dump(GetRequest.serializer(), request)),
+					Serialization.PROTOBUF.mediaType
+				)
+			}
+			val url = "$baseUrl/UserService/getUser"
+			val httpResponse = httpClient.send(HttpRequest(url, HttpMethod.POST, body))
+			if (httpResponse.status.successful) {
+				val responseBody = httpResponse.body!!.content
+				return when (serialization) {
+					Serialization.JSON -> JSON.parse(GetResponse.serializer(), responseBody)
+					Serialization.PROTOBUF -> ProtoBuf.load(GetResponse.serializer(), Base64.getDecoder().decode(responseBody))
+				}
+			} else {
+				// TODO parse FailureException
+				throw Exception()
+			}
+		}
+	}
+}
+
+// TODO no two function can have the same name
+// TODO handle number of arguments = 0?
+fun UserService.Companion.binder(implementation: UserService): ServiceHandler {
+	return object : ServiceHandler {
+		override val serviceName: String = "UserService"
+
+		override fun rpcHandler(rpc: String): RpcHandler<*, *>? {
+			return when (rpc) {
+				"getUser" -> object : RpcHandler<GetRequest, GetResponse> {
+					override val requestLoader = GetRequest.serializer()
+					override val responseSaver = GetResponse.serializer()
+					override suspend fun run(request: GetRequest): GetResponse = implementation.getUser(request)
+				}
+				else -> null
+			}
+		}
+	}
+}
+
+enum class Serialization(val mediaType: String) {
+	JSON("application/json"),
+	PROTOBUF("application/octet-stream")
+}
+
+interface RpcHandler<I : Any, O : Any> {
+	val requestLoader: KSerialLoader<I>
+
+	val responseSaver: KSerialSaver<O>
+
+	suspend fun run(request: I): O
+}
+
+interface ServiceHandler {
+	val serviceName: String
+
+	fun rpcHandler(rpc: String): RpcHandler<*, *>?
+}
+
+class Server(
+	port: Int = 80,
+	host: String = "0.0.0.0"
+) : ApplicationEngine {
+
+	private companion object {
+		const val serviceArg = "service"
+		const val rpcArg = "rpc"
 	}
 
-	@RequestMapping(method = [RequestMethod.GET])
-	fun getAllusers(): List<User?> = listOf()
+	private val serviceBinders = hashMapOf<String, ServiceHandler>()
+	private val engine = embeddedServer(Netty, port, host) {
+		routing {
+			post("/{$serviceArg}/{$rpcArg}") {
+				handleCall(context)
+			}
+		}
+	}
 
-	@PutMapping
-	fun saveUser(@RequestBody user: User): DeferredResult<Unit> = DeferredResult()
+	override val environment: ApplicationEngineEnvironment
+		get() = engine.environment
+
+	override fun start(wait: Boolean): ApplicationEngine = engine.start(wait)
+
+	override fun stop(gracePeriod: Long, timeout: Long, timeUnit: TimeUnit) {
+		engine.stop(gracePeriod, timeout, timeUnit)
+	}
+
+	fun bindService(serviceHandler: ServiceHandler) {
+		val name = serviceHandler.serviceName
+		if (serviceBinders.containsKey(name)) {
+			throw IllegalArgumentException("Server already bound to service named $name")
+		}
+		serviceBinders[name] = serviceHandler
+	}
+
+	private suspend fun handleCall(call: ApplicationCall) {
+		// Get service
+		val service = serviceBinders[call.parameters[serviceArg]!!]
+		if (service == null) {
+			call.respond(HttpStatusCode.NotFound, "")
+			return
+		}
+
+		// Get handler
+		val handler = service.rpcHandler(call.parameters[rpcArg]!!)
+		if (handler == null) {
+			call.respond(HttpStatusCode.NotFound, "")
+			return
+		}
+
+		// Call handler
+		runHandler(handler, call)
+	}
+
+	private suspend fun <I : Any, O : Any> runHandler(handler: RpcHandler<I, O>, call: ApplicationCall) {
+		// TODO use Accept header
+		val contentType = call.request.contentType()
+		val mediaType = "${contentType.contentType}/${contentType.contentSubtype}"
+
+		val requestBody = call.receiveChannel().toInputStream()
+			.bufferedReader(call.request.contentCharset() ?: Charsets.UTF_8)
+			.use { it.readText() }
+		val request = when (mediaType) {
+			Serialization.JSON.mediaType -> JSON.parse(handler.requestLoader, requestBody)
+			Serialization.PROTOBUF.mediaType -> ProtoBuf.load(
+				handler.requestLoader,
+				Base64.getDecoder().decode(requestBody)
+			)
+			else -> {
+				call.respond(
+					HttpStatusCode.UnsupportedMediaType,
+					"Media type '$mediaType' is not supported. Must be either '${Serialization.JSON.mediaType}' or '${Serialization.JSON.mediaType}'."
+				)
+				return
+			}
+		}
+
+		val response = handler.run(request)
+		when (mediaType) {
+			Serialization.JSON.mediaType -> {
+				val json = JSON.stringify(handler.responseSaver, response)
+				call.respondText(json, ContentType.Application.Json)
+			}
+			Serialization.PROTOBUF.mediaType -> {
+				val content = Base64.getEncoder().encodeToString(ProtoBuf.dump(handler.responseSaver, response))
+				call.respondText(content, ContentType.Application.OctetStream)
+			}
+		}
+	}
 }
 
 fun main(args: Array<String>) {
-	val funSpec = FunSpec.builder("myFunction")
-	val parameter = ParameterSpec.builder("body", Int::class).build()
-	funSpec.addParameter(parameter)
-	val nameAllocator = NameAllocator()
-	nameAllocator.newName(parameter.name, parameter)
-	nameAllocator.newName("body", "body")
+	val port = 8080
+	Server(port = port).run {
+		bindService(UserService.binder(UserServiceImpl))
+		start()
+	}
 
-	funSpec.addStatement("val %L = %L", nameAllocator.get("body"), 3)
-	funSpec.addStatement("println(%N)", nameAllocator.get(parameter))
-	FileSpec.builder("com.example", "MyFile")
-		.addFunction(funSpec.build())
-		.build().writeTo(System.out)
+	val httpClient = OkHttpClient(okhttp3.OkHttpClient())
+	val userService = UserService.create(httpClient, "http://127.0.0.1:$port")
+	runBlocking {
+		println(userService.getUser(GetRequest(42)))
+	}
 }
